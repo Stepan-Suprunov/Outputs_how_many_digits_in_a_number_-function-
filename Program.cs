f6daef3609d1d7411864d61ca064542260a63126
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-06-19 02:50:51 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Выдаёт колличество цифр в числе.
+
+int Func(int num)
+{
+    int quantity = 0;
+    for (; num >= 1; quantity++)
+    {
+        num = num/10;
+    }
+    return quantity;
+}
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+if (num == 0)
+{
+    Console.WriteLine("Колличество цифр в числе: 1");
+}
+else if (num < 0)
+{
+    num = num * -1;
+    Console.WriteLine("Колличество цифр в числе: " + Func(num));
+}
+else
+{
+Console.WriteLine("Колличество цифр в числе: " + Func(num));
+}
